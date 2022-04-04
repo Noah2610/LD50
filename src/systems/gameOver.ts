4d7a7187be9b40a6eefbec7c8cecae9ca94d2a9e
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2022-04-05 01:25:37 +0200

    Simple game over UI

--- src/systems/gameOver.ts
+++ src/systems/gameOver.ts
@@ -1,9 +1,14 @@
+import { expectEl } from "../util";
 import { GameContext } from "../context";
 import { query } from "../query";
 
 export function gameOver(ctx: GameContext) {
+    if (ctx.resources.isGameOver) return;
+
     const isGameOver = query(ctx, ["Turret"]).next().done;
     if (isGameOver) {
         ctx.resources.isGameOver = true;
+        const gameOverEl = expectEl("#game .game-over");
+        gameOverEl.classList.remove("hidden");
     }
 }
